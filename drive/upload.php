24a721aa911cb570ce909cdf8c0a0179632a2573
ImagePuzzle <musicinme9999@gmail.com>
2020-01-21 17:55:57 +0200

    file uploader

--- drive/upload.php
+++ drive/upload.php
@@ -4,27 +4,22 @@ define('Gugol', TRUE);
 include '../engine/utils/RandomString.php';
 
 $target_dir = "../../drive/";
-$target_file = $target_dir . basename($_FILES["fileid"]["name"]);
+$target_file = $target_dir . basename($_FILES["filename"]["name"]);
 $uploadOk = 1;
-$newName = $target_file . generateRandomString(20);
+$newName = $target_file . generateRandomString(20) . '.att';
 
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+if ($_FILES["filename"]["size"] > 1024) {
+    echo "Sorry, your file is too large. ";
     $uploadOk = 0;
 }
-// Check file size
-if ($_FILES["fileid"]["size"] > 1024) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+    echo "Your file was not uploaded.";
+
 } else {
-    if (move_uploaded_file($_FILES["fileid"]["tmp_name"], $newName)) {
-        echo "The file ". basename( $_FILES["fileid"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["filename"]["tmp_name"], $newName)) {
+        echo "The file ". basename( $_FILES["filename"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+header("Location: index.php");
